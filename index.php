c2e988309d37d1d6869f785576720d0f40e671cb
albvazq <alb.vazq@gmail.com>
2013-06-24 00:51:37 -0500

    Regreso al index original
    Modificar método de errutado del request.

--- index.php
+++ index.php
@@ -7,4 +7,130 @@
 	bootstrapper::init();
 
 	
-	echo testList::getValue("mx");
+	/*echo "<pre>";
+	print_r($_SERVER);
+	echo "</pre>";*/
+
+	if(isset($_SERVER['ORIG_PATH_INFO']))
+		$components = explode("/", substr($_SERVER['ORIG_PATH_INFO'], 1));
+	else
+		$components = explode("/", substr($_SERVER['PATH_INFO'], 1));
+	
+
+	if($components[0] == "") $components = null;
+
+	switch(count($components))
+
+	{
+
+		case 0:
+
+			//Index
+
+			
+
+			break;
+
+		case 1:
+
+			//Action index controller
+
+			$controller = strtolower($components[0])."Controller";
+
+			if (class_exists($controller)) {
+
+			    $controller = new $controller();
+
+				$controller->baseName = strtolower($components[0]);
+
+				$controller->index();
+
+			} else{
+
+				echo "Controller {$controller} no existe";
+
+			}
+
+			break;
+
+		case 2:
+
+			$controller = strtolower($components[0])."Controller";
+
+			if (class_exists($controller)) {
+
+			    $controller = new $controller();
+
+				$controller->baseName = strtolower($components[0]);
+
+				if(method_exists($controller, $components[1])){
+
+					$controller->$components[1]();
+
+				} else{
+
+					echo "No puede ejecutarse la accion {$components[1]}";
+
+				}
+
+				
+
+			} else{
+
+				echo "Controller {$controller} no existe";
+
+			}
+
+			//Action controller
+
+			break;
+
+		default:
+
+			//Parameter for action
+
+			$controller = strtolower($components[0])."Controller";
+
+			if (class_exists($controller)) {
+
+			    $controller = new $controller();
+
+				$controller->baseName = strtolower($components[0]);
+
+				if(method_exists($controller, $components[1])){
+
+					$i = 2;
+
+					$p = array();
+
+					while($components[$i])
+
+					{
+
+						$p[] = $components[$i];
+
+						$i++;
+
+					}
+
+					call_user_func_array(array($controller, $components[1]), $p);
+
+					unset($p);
+
+				} else{
+
+					echo "No puede ejecutarse la accion {$components[1]}";
+
+				}
+
+				
+
+			} else{
+
+				echo "Controller {$controller} no existe";
+
+			}
+
+			break;
+
+	}
