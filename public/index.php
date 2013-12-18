99ad589aa737bac2694ce056c1ee1bc0babe43a3
Sergio Alberto Vázquez <alb.vazq@gmail.com>
2013-12-17 20:12:08 -0600

    Cambios en el index.php
    Estos cambios evitan la aparición de warnings por falta de parámetros en
    la url

--- public/index.php
+++ public/index.php
@@ -9,14 +9,19 @@
 	if(isset($_SERVER['ORIG_PATH_INFO']))
 		$components = explode("/", substr($_SERVER['ORIG_PATH_INFO'], 1));
 	else
+            if(isset ($_SERVER['PATH_INFO']))
 		$components = explode("/", substr($_SERVER['PATH_INFO'], 1));
 	
-
-	if(trim($components[0]) == "") {
-		$components[0] = "main";
-	}
-	$controller = strtolower($components[0])."Controller";
-
+        if(isset($components[0])) {
+            if(trim($components[0]) == "") {
+                    $controller = "main";
+            } else {
+                $controller = $components[0];
+            }
+        } else {
+            $controller = "main";
+        }
+        $controller = strtolower($controller)."Controller";
 	if (class_exists($controller)) {
 		$controller = new $controller();
 		$controller->execute();
